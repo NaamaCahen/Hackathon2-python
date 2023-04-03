47805f1704e8195a7d1b715b72dce8836d50d57e
NaamaCahen <cahenbb@gmail.com>
2023-04-03 21:39:20 +0300

    adding a task works!!

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,17 @@
 import flask
 
-from app import flask_app
+from app import flask_app,db
 from .forms import AddTodo
+from .models import Tasks
 
 
-@flask_app.route('/')
+@flask_app.route('/',methods=('GET','POST'))
 def home():
     form = AddTodo()
-    return flask.render_template('index.html', form=form)
+    if form.validate_on_submit():
+        new_task = Tasks(description=form.description.data, category=form.category.data, deadline=form.deadline.data,
+                         is_urgent=form.is_urgent.data)
+        new_task.save_task_to_db()
+        return flask.redirect('/')
+    tasks = Tasks.get_tasks()
+    return flask.render_template('index.html', form=form, tasks=tasks)
